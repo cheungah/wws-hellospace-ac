3b036d771fa748bf45029ac69bf3986352455375
Allen Cheung <cheungah@hk1.ibm.com>
2017-10-19 14:53:25 +0000

    ok

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var bodyParser = require("body-parser");
 var request = require("request");
 var requestjs = require("request-json");
 
-var APP_ID = "a94753f5-???";
-var APP_SECRET = "4UZLpTis-??";
-var SPACE_ID = "5819???";
+var APP_ID = "254ac3e4-76fa-4ddb-b355-e04552278b04";
+var APP_SECRET = "w_yssM6_WHdwBGNW-M7MenejMjEQ";
+var SPACE_ID = "59e71446e4b015437cf48db2";
 
 // --------------------------------------------------------------------------
 // Setup global variables
@@ -36,7 +36,7 @@ var jsonParser = bodyParser.json();
 // --------------------------------------------------------------------------
 // Start our server !
 app.listen(process.env.PORT || 3000, function() {
-    console.log("INFO: app is listening on port %s", (process.env.PORT || 3000));
+	console.log("INFO: app is listening on port %s", (process.env.PORT || 3000));
 });
 
 
@@ -44,44 +44,87 @@ app.listen(process.env.PORT || 3000, function() {
 // --------------------------------------------------------------------------
 // REST API test : listen for GET requests on /inspiration
 app.get("/inspiration", function(req, res) {
-    console.log(req.body);
-    res.send("Are you looking for inspiration?");
+	console.log(req.body);
+	// Build msg from query Parameter
+	var myMsg = req.query.msg;
+
+	// Let's try to authenticate
+	getJWTToken(APP_ID, APP_SECRET, function(jwt) {
+		console.log("JWT Token :", jwt);
+		postMessageToSpace(SPACE_ID, jwt, myMsg, function(success) {
+			if (success) {
+				res.status(200).end();
+			} else {
+				res.status(500).end();
+			}
+		});
+	});
+	res.send("Are you looking for inspiration?");
 });
 
 //--------------------------------------------------------------------------
 //Get an authentication token
 function getJWTToken(userid, password, callback) {
-    // Build request options for authentication.
-    const authenticationOptions = {
-        "method": "POST",
-        "url": `${WWS_URL}${AUTHORIZATION_API}`,
-        "auth": {
-            "user": userid,
-            "pass": password
-        },
-        "form": {
-            "grant_type": "client_credentials"
-        }
-    };
+	// Build request options for authentication.
+	const authenticationOptions = {
+		"method": "POST",
+		"url": `${WWS_URL}${AUTHORIZATION_API}`,
+		"auth": {
+			"user": userid,
+			"pass": password
+		},
+		"form": {
+			"grant_type": "client_credentials"
+		}
+	};
 
-    // Get the JWT Token
-    request(authenticationOptions, function(err, response, authenticationBody) {
+	// Get the JWT Token
+	request(authenticationOptions, function(err, response, authenticationBody) {
 
-        // If successful authentication, a 200 response code is returned
-        if (response.statusCode !== 200) {
-            // if our app can't authenticate then it must have been
-            // disabled. Just return
-            console.log("ERROR: App can't authenticate");
-            callback(null);
-        }
-        const accessToken = JSON.parse(authenticationBody).access_token;
-        callback(accessToken);
-    });
+		// If successful authentication, a 200 response code is returned
+		if (response.statusCode !== 200) {
+			// if our app can't authenticate then it must have been
+			// disabled. Just return
+			console.log("ERROR: App can't authenticate");
+			callback(null);
+		}
+		const accessToken = JSON.parse(authenticationBody).access_token;
+		callback(accessToken);
+	});
 }
 
 //--------------------------------------------------------------------------
 //Post a message to a space
 function postMessageToSpace(spaceId, accessToken, textMsg, callback) {
 
+	var jsonClient = requestjs.createClient(WWS_URL);
+	var urlToPostMessage = "/v1/spaces/" + spaceId + "/messages";
+	jsonClient.headers.jwt = accessToken;
 
+	// Building the message
+	var messageData = {
+		type: "appMessage",
+		version: 1.0,
+		annotations: [{
+			type: "generic",
+			version: 1.0,
+			color: "#00B6CB",
+			text: textMsg
+		}]
+	};
+
+	// Calling IWW API to post message
+	console.log("Message body : %s", JSON.stringify(messageData));
+
+	jsonClient.post(urlToPostMessage, messageData, function(err, jsonRes, jsonBody) {
+		if (jsonRes.statusCode === 201) {
+			console.log("Message posted to IBM Watson Workspace successfully!");
+			callback(true);
+		} else {
+			console.log("Error posting to IBM Watson Workspace !");
+			console.log("Return code : " + jsonRes.statusCode);
+			console.log(jsonBody);
+			callback(false);
+		}
+	});
 }
